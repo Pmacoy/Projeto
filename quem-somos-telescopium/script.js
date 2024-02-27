8fdaf4e3cf8d0bfac5b2ba074b85aa7a48220e05
lunna <150831224+taecisne@users.noreply.github.com>
2024-02-27 16:37:15 -0300

    atualização

---
+++ quem-somos-telescopium/script.js
@@ -0,0 +1,11 @@
+document.getElementById("quemSomos").addEventListener("click", function() {
+    window.location.href = "link_para_quem_somos.html";
+});
+
+document.getElementById("filmesSeries").addEventListener("click", function() {
+    window.location.href = "link_para_filmes_series.html";
+});
+
+document.getElementById("podcasts").addEventListener("click", function() {
+    window.location.href = "link_para_podcasts.html";
+});
